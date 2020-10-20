df3ce316ebe5a3a0ccc5ee89115cd531e35369fa
maluudias <73119774+maluudias@users.noreply.github.com>
2020-10-20 11:51:28 -0300

    2017098048
    1) Coloquei a quantidade de horas trabalhadas como double
    2) Agrupei as informações dos vendedores, tirando os espaços que haviam entre a informação e o cout
    3) Aloquei dinamicamente os engenheiros e vendedores. uma vez que as informações podem ser recolhidas nas classes

--- main.cpp
+++ main.cpp
@@ -3,56 +3,38 @@
 
 int main() {
 
-  Engenheiro eng1;
-  eng1.nome = "Joao Snow";
-  eng1.salarioHora = 35.00;
-  eng1.projetos = 3; 
+  Engenheiro *eng1 = new Engenheiro();
   std::cout << "Nome: " << eng1.nome << std::endl;
   std::cout << "Salario Mes: " << eng1.pagamentoMes(9.50) << std::endl;
   std::cout << "Projetos: " << eng1.projetos << std::endl;
   std::cout << std::endl;
   
-  Engenheiro eng2;
-  eng2.nome = "Daniela Targaryen";
-  eng2.salarioHora = 30.00;
-  eng2.projetos = 1; 
+ Engenheiro *eng2 = new Engenheiro();
   std::cout << "Nome: " << eng2.nome << std::endl;
   std::cout << "Salario Mes: " << eng2.pagamentoMes(8.00) << std::endl;
   std::cout << "Projetos: " << eng2.projetos << std::endl;  
   std::cout << std::endl;
   
-  Engenheiro eng3;
-  eng3.nome = "Bruno Stark";
-  eng3.salarioHora = 30.00;
-  eng3.projetos = 2; 
+  Engenheiro *eng3 = new Engenheiro();
   std::cout << "Nome: " << eng3.nome << std::endl;
   std::cout << "Salario Mes: " << eng3.pagamentoMes(8.00) << std::endl;
   std::cout << "Projetos: " << eng3.projetos << std::endl;  
   std::cout << std::endl;
   
   
-  Vendedor vend1;
-  vend1.nome = "Tonho Lannister";
-  vend1.salarioHora = 20.00;
-  vend1.quotaMensalVendas = 5000.00;
+  Vendedor *vend1 = new Vendedor();
   std::cout << "Nome: " << vend1.nome << std::endl;
   std::cout << "Salario Mes: " << vend1.pagamentoMes(6.00) << std::endl;  
   std::cout << "Quota vendas: " << vend1.quotaTotalAnual() << std::endl;
   std::cout << std::endl;
   
-  Vendedor vend2;
-  vend2.nome = "Jose Mormont";
-  vend2.salarioHora = 25.00;
-  vend2.quotaMensalVendas = 3000.00;
+ Vendedor *vend2 = new Vendedor();
   std::cout << "Nome: " << vend2.nome << std::endl;
   std::cout << "Salario Mes: " << vend2.pagamentoMes(8.00) << std::endl;  
   std::cout << "Quota vendas: " << vend2.quotaTotalAnual() << std::endl; 
   std::cout << std::endl;  
 	
-  Vendedor vend3;
-  vend3.nome = "Sonia Stark";
-  vend3.salarioHora = 30.00;
-  vend3.quotaMensalVendas = 4000.00;
+  Vendedor *vend3 = new Vendedor();
   std::cout << "Nome: " << vend3.nome << std::endl;
   std::cout << "Salario Mes: " << vend3.pagamentoMes(8.00) << std::endl;  
   std::cout << "Quota vendas: " << vend3.quotaTotalAnual() << std::endl;  
